fec01840e7653ff55e7988c3cd5718ab9bcd6fbb
Everton Cordeiro <evertoncordeiro@icloud.com>
2024-08-15 22:24:08 -0300

    exclusão de linhas em brancas desnecessárias

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
-
     const $app = $('<div></div>')
         .addClass('flex flex-col items-center w-full max-w-md mx-auto')
 
     const $header = $('<div></div>')
         .addClass('flex items-center gap-x-4')
-
 
     const $title = $('<h1></h1>')
         .text('Lista de Tarefas')
@@ -13,30 +11,26 @@ $(document).ready(function () {
 
     const $themeButton = $('<button></button>')
         .addClass('p-2 text-blue-500')
-        .html('<i id="themeButton" class="fa-solid fa-moon w-6 h-6"></i>').attr('title','trocar para modo escuro')
-
-        .addClass('p-2')
+        .html('<i id="themeButton" class="fa-solid fa-moon w-6 h-6"></i>')
+        .attr('title', 'trocar para modo escuro')
         .on('click', function () {
             const $icon = $('#themeButton')
-
             if ($icon.hasClass('fa-moon')) {
-                $icon.removeClass('fa-moon').addClass('fa-sun').css('color', 'yellow'),
-                $('body').addClass('bg-neutral-800 text-white').removeClass('bg-slate-100 text-black'),
-                $themeButton.attr('title','trocar para modo claro')
+                $icon.removeClass('fa-moon').addClass('fa-sun').css('color', 'yellow')
+                $('body').addClass('bg-neutral-800 text-white').removeClass('bg-slate-100 text-black')
+                $themeButton.attr('title', 'trocar para modo claro')
             } else {
-                $icon.removeClass('fa-sun').addClass('fa-moon').css('color', 'blue'),
-                $('body').addClass('bg-slate-100 text-black').removeClass('bg-neutral-800 text-white'),
-                $themeButton.attr('title','trocar para modo escuro')
+                $icon.removeClass('fa-sun').addClass('fa-moon').css('color', 'blue')
+                $('body').addClass('bg-slate-100 text-black').removeClass('bg-neutral-800 text-white')
+                $themeButton.attr('title', 'trocar para modo escuro')
             }
         })
 
-
     $header.append($title, $themeButton)
-
     $app.append($header)
 
     const $inputDiv = $('<div></div>')
-        .addClass('flex gap-2 mb-4 ')
+        .addClass('flex gap-2 mb-4')
 
     const $taskInput = $('<input>')
         .attr('id', 'task-input')
@@ -56,58 +50,47 @@ $(document).ready(function () {
         .attr('id', 'task-list')
         .addClass('w-full bg-transparent')
 
-
-
     $app.append($taskList)
-
     $('body').append($app)
 
     function addTask() {
         const taskText = $('#task-input').val().trim()
-    
         if (taskText) {
-          const $listItem = $('<li></li>')
-            .addClass('flex justify-between items-center border border-gray-300 rounded p-2 mb-2 bg-transparent')
-    
-          const $taskDiv = $('<div></div>')
-            .addClass('text-lg bg-transparent w-3/4 overflow-hidden text-ellipsis whitespace-nowrap')
-            .text(taskText)
-            .on('click', () => $taskDiv.toggleClass('line-through'))
-            .css('cursor', 'pointer')
-    
-          const $deleteButton = $('<button></button>')
-            .html('<i class="fa-solid fa-trash-can" style="color: #ff1900; width: 1.5em; height: 1.5em;"></i>')
-            .addClass('px-3 py-1 rounded')
-            .on('click', () => $listItem.remove())
-    
-          $listItem.append($taskDiv, $deleteButton)
-          $('#task-list').append($listItem)
-          $('#task-input').val('')
+            const $listItem = $('<li></li>')
+                .addClass('flex justify-between items-center border border-gray-300 rounded p-2 mb-2 bg-transparent')
+
+            const $taskDiv = $('<div></div>')
+                .addClass('text-lg bg-transparent w-3/4 overflow-hidden text-ellipsis whitespace-nowrap')
+                .text(taskText)
+                .on('click', () => $taskDiv.toggleClass('line-through'))
+                .css('cursor', 'pointer')
+
+            const $deleteButton = $('<button></button>')
+                .html('<i class="fa-solid fa-trash-can" style="color: #ff1900; width: 1.5em; height: 1.5em;"></i>')
+                .addClass('px-3 py-1 rounded')
+                .on('click', () => $listItem.remove())
+
+            $listItem.append($taskDiv, $deleteButton)
+            $('#task-list').append($listItem)
+            $('#task-input').val('')
         }
-      }
-    
-      $('#add-task').on('click', addTask)
-    
-      $('#task-input').on('keypress', function(e) {
-        if (e.which === 13) { 
-          e.preventDefault() 
-          addTask() 
+    }
+
+    $('#add-task').on('click', addTask)
+
+    $('#task-input').on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault()
+            addTask()
         }
-      })
-      
+    })
+
     const $footer = $('<footer></footer>')
         .addClass('text-center')
 
     const $textFooter = $('<h1></h1>')
         .text('CODE BY ÉVERTON CORDEIRO')
         .addClass('text-center text-1xl text-blue-300 text-opacity-4')
-
-    $('body').append(
-        $footer.append(
-            $textFooter
-        )
-    )
+    
+    $('body').append($footer.append($textFooter))
 })
-
-
-
